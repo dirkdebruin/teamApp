fe1e40da972049d351e2c03e02d505ae11e4237e
dirkdebruin <d.de.bruin@portbase.com>
2022-12-12 17:11:49 +0100

    delete default and added angular materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, 
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
